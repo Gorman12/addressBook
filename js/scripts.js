08d5a89e25ebaa64123c8e5d48cb06883d6859ca
cam hering and sean gorman <cam@>
2016-10-12 18:42:30 -0700

    includes address function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
   return (this.firstName + " " + this.lastName);
 }
+
 
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
